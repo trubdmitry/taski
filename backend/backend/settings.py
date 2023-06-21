6f5f5046a93a88b3fbc62c2e925a6ed0f4031553
trubdmitry <tr.dmitrii@mail.ru>
2023-06-21 10:12:10 +0000

    ALLOWED_HOSTS add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.24.99','127.0.0.1' , 'localhost']
 
 
 # Application definition
