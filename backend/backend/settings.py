79e5ebe928f4603adcd49b353cac512907ab53bc
trubdmitry <tr.dmitrii@mail.ru>
2023-06-21 11:44:44 +0000

    ALLOWED_HOSTS add DNS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.24.99','127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['trubdmitry.hopto.org', '51.250.24.99','127.0.0.1' , 'localhost']
 
 
 # Application definition
